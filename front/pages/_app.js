46fa7d3a43fe87a617a04f7a22518163fd5c55d0
Bum <sbl133@naver.com>
2021-07-03 00:11:10 +0900

    feat: _app.js, fix: eslint error

---
+++ front/pages/_app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import PropTypes from 'prop-types';
+
+const App = ({ Component }) => {
+  return (
+    <>
+      <Component />
+    </>
+  );
+};
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default App;
